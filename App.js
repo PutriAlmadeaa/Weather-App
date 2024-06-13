4361c70a043abe07e38e2b3efa25ee49f136e06f
PutriAlmadeaa <almadeaputri20@gmail.com>
2024-06-13 23:31:02 +0700

    Menampilkan Data Cuaca

--- App.js
+++ App.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { BASE_URL, API_KEY } from './src/constant'
 import { View, StyleSheet } from 'react-native'
 import WeatherSearch from './src/components/weatherSearch'
 import WeatherInfo from './src/components/weatherInfo'
 
 const App = () => {
+  const[weatherData, setweatherData] = useState()
+
+  const searchWeather = (location) =>{
+    const url = `${BASE_URL}?q=${location}&appid=${API_KEY}`
+    console.log(`Request URL: ${url}`)
+
+    axios
+    .get(`${BASE_URL}?q=${location}&appid=${API_KEY}`)
+    .then((response)=>{
+      const data = response.data
+      data.visibility /= 1000
+      data.visibility = data.visibility.toFixed(2)
+      data.main.temp -= 273.15 
+      data.main.temp = data.main.temp.toFixed(2)
+      setweatherData(data)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <WeatherSearch />
-      <WeatherInfo />
+      <WeatherSearch searchWeather={searchWeather} />
+      {/* Tampilkan data cuaca ketika ada weatherData */}
+      {weatherData && <WeatherInfo weatherData={weatherData} />}
     </View>
   )
 }
---
+++ src/constant.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+export const API_KEY = 'fa0229597fd394490c7fa5049aaaf0e1';
